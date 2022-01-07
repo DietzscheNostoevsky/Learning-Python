6bdfb51af1634832cb24cdfdc122c38948a5850f
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-01-07 19:05:14 +0530

    .

--- General-Function-Codes/WeightGraph.py
+++ General-Function-Codes/WeightGraph.py
@@ -3,12 +3,11 @@ y = 0
 z = 0
 
 
-
 for x in range(15):
-    y = 83*(0.99)**x
+    y = 83*(0.995)**x
     #z = int(y)
-    z = round(y,1)
+    z = round(y, 1)
+
     #print (x,":",y)
     #print(x,':', z, ':', y)
-    print(x,':',z)
-
+    print(x, ':', z)
