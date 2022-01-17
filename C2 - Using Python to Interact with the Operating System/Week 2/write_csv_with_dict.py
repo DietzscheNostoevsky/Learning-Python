89243d61be2564777f229af35ddd1b6f88aa4837
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-01-17 18:43:50 +0530

    c 2 week 2 dir

---
+++ C2 - Using Python to Interact with the Operating System/Week 2/write_csv_with_dict.py
@@ -0,0 +1,26 @@
+# %%
+import os
+import csv
+
+# %%
+with open("software.csv") as software:
+    reader1 = csv.DictReader(software)
+    for row in reader1:
+        print(("{} has {} users").format(row["name"], row["users"]))
+
+# %%
+
+users = [{"name": "Saurabh Singh", "username": "SKS", "department": "IT Infra"},
+         {"name": "Junglee Kumar", "username": "JKK", "department": "User Exp res"},
+         {"name": "Piggy Piglet", "username": "PPG", "department": "developement"}]
+
+keys = ["name", "username", "department"]
+
+
+# %%
+
+with open("by_dept.csv", "w") as dept:
+    writer1 = csv.DictWriter(dept, fieldnames=keys)
+    writer1.writeheader()
+    writer1.writerows(users)
+# %%
