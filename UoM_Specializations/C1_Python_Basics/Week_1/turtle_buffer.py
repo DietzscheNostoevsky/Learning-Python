b5fd3d6eb78ac470e2ca42c696f902bfb4a02436
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-01-29 13:34:49 +0530

    turtle pentagram done

---
+++ UoM_Specializations/C1_Python_Basics/Week_1/turtle_buffer.py
@@ -0,0 +1,15 @@
+import turtle
+
+# Creating a general purpose code whcih asks user to enter
+# number of side in the regular polygon and creates a polygon of that side
+
+wn = turtle.Screen()
+kachua = turtle.Turtle()
+
+#side = int(input("Enter the number of sides in the polygon :"))
+
+# angle = 360/side  # calculates the angle turtle have to turn
+
+kachua.forward(100)
+
+wn.exitonclick()
---
+++ UoM_Specializations/C1_Python_Basics/Week_1/turtle_pentagram.py
@@ -0,0 +1,10 @@
+import turtle
+
+wn = turtle.Screen()
+kachua = turtle.Turtle()
+
+for i in range(5):
+    kachua.forward(100)
+    kachua.right(144)
+
+wn.exitonclick()
