7e2a105ebdb6f05ae516189d5cc1bc02faf51441
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-01-31 14:51:47 +0530

    function get_neg working

--- UoM_Specializations/C2_Python Functions_Files_and_Dictionaries/Week_5/sentiment_classifier.py
+++ UoM_Specializations/C2_Python Functions_Files_and_Dictionaries/Week_5/sentiment_classifier.py
@@ -28,5 +28,19 @@ def get_pos(sentence):
     return count
 
 
-testing = "what a truly wonderful day it is today! #incredible"
-print(get_pos(testing))
+negative_words = []
+with open("negative_words.txt") as pos_f:
+    for lin in pos_f:
+        if lin[0] != ';' and lin[0] != '\n':
+            negative_words.append(lin.strip())
+
+
+def get_neg(sentence):
+    new = strip_punctuation(sentence)
+    wordlist = new.split()
+    count = 0
+    for w in wordlist:
+
+        if w.lower() in negative_words:
+            count += 1
+    return count
