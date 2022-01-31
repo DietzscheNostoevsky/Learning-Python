49d4d3a2a5896da1c013a3bd964af16274a50e65
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-01-31 14:45:47 +0530

    def_pos function working

--- UoM_Specializations/C2_Python Functions_Files_and_Dictionaries/Week_5/sentiment_classifier.py
+++ UoM_Specializations/C2_Python Functions_Files_and_Dictionaries/Week_5/sentiment_classifier.py
@@ -10,4 +10,23 @@ def strip_punctuation(word):
     return input_string
 
 
-strip_punctuation("#Amazing")
+positive_words = []
+with open("positive_words.txt") as pos_f:
+    for lin in pos_f:
+        if lin[0] != ';' and lin[0] != '\n':
+            positive_words.append(lin.strip())
+
+
+def get_pos(sentence):
+    new = strip_punctuation(sentence)
+    wordlist = new.split()
+    count = 0
+    for w in wordlist:
+
+        if w.lower() in positive_words:
+            count += 1
+    return count
+
+
+testing = "what a truly wonderful day it is today! #incredible"
+print(get_pos(testing))
