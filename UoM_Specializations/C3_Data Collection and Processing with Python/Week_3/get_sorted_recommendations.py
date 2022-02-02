d6dc7e185e52ee48d069380bb64c3b2c59acf770
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-02-02 22:46:05 +0530

    done getting recommendations, returning movie list sorted

---
+++ UoM_Specializations/C3_Data Collection and Processing with Python/Week_3/get_sorted_recommendations.py
@@ -0,0 +1,37 @@
+# -----------------------------------------------------------------------
+# Define a function get_sorted_recommendations.
+# It takes a list of movie titles as an input.
+# It returns a sorted list of related movie titles as output,
+# up to five related movies for each input movie title.
+# The movies should be sorted in descending order by their
+# Rotten Tomatoes rating, as returned by the get_movie_rating function.
+# Break ties in reverse alphabetic order,
+# so that ‘Yahşi Batı’ comes before ‘Eyyvah Eyvah’.
+# -----------------------------------------------------------------------
+# %%
+
+import requests
+
+from Project_OMDB_and_TasteDive import *
+
+# -----------------------------------------------------------------------
+#get_sorted_recommendations(["Bridesmaids", "Sherlock Holmes"])
+
+# %%
+
+
+def get_sorted_recommendations(movielist):
+
+    # %%
+    relatedmovies = get_related_titles(movielist)
+    retlist: []
+    moviedict = {}
+    for movie in relatedmovies:
+        rating = get_movie_rating(get_movie_data(movie))
+
+        moviedict[movie] = rating
+    sortedmoviedict = dict(
+        sorted(moviedict.items(), key=lambda k: (k[1], k[0]), reverse=True))
+    retlist = list(sortedmoviedict.keys())
+
+    return retlist
