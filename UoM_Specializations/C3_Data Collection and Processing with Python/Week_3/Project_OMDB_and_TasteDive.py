14c0d8c2e1876ccdc56e1ac2b95d73eb0e3fbf99
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-02-02 19:37:42 +0530

    get related titles working

--- UoM_Specializations/C3_Data Collection and Processing with Python/Week_3/Project_OMDB_and_TasteDive.py
+++ UoM_Specializations/C3_Data Collection and Processing with Python/Week_3/Project_OMDB_and_TasteDive.py
@@ -43,6 +43,22 @@ def extract_movie_titles(indict):
         movielist.append(movie)
     return movielist
 # -----------------------------------------------------------------------
+# you’ll write a function, called get_related_titles.
+# It takes a list of movie titles as input.
+# It gets five related movies for each from TasteDive,
+# extracts the titles for all of them,
+# and combines them all into a single list.
+# Don’t include the same movie twice.
 
 
-# %%
+def get_related_titles(inlist):
+    retlist = []
+    for movie in inlist:
+        movielist = extract_movie_titles(get_movies_from_tastedive(movie))
+        for m in movielist:
+            if m in retlist:
+                continue
+            else:
+                retlist.append(m)
+    return retlist
+# -----------------------------------------------------------------------
