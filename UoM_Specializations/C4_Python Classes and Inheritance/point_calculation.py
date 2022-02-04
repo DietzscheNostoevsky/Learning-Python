bf12f2dafeffaeed2e43f7333ba88ca9a111a9d2
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-02-04 16:33:42 +0530

    added graph for point

--- UoM_Specializations/C4_Python Classes and Inheritance/point_calculation.py
+++ UoM_Specializations/C4_Python Classes and Inheritance/point_calculation.py
@@ -3,6 +3,7 @@
 # %%
 class Point:
     """ Point class for representing and manipulating x,y coordinates. """
+    printed_rep = "*"
 
     def __init__(self, initx, inity):
         self.x = initx
@@ -41,5 +42,22 @@ class Point:
         retpt = Point(xd, yd)
         return retpt
 
+    def graph(self):
+        rows = []
+        size = max(int(self.x), int(self.y)) + 2
+        for j in range(size-1):
+            if (j+1) == int(self.y):
+                special_row = str((j+1) % 10) + \
+                    (" "*(int(self.x) - 1)) + self.printed_rep
+                rows.append(special_row)
+            else:
+                rows.append(str((j+1) % 10))
+        rows.reverse()  # put higher values of y first
+        x_axis = ""
+        for i in range(size):
+            x_axis += str(i % 10)
+        rows.append(x_axis)
+
+        return "\n".join(rows)
 
 # %%
