20fca2f1dfec0be735363e3c5f9e14faac125f55
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-02-05 20:42:51 +0530

    unit test module for rearrange function

--- Google_IT_Automation_Specialization/C2 - Using Python to Interact with the Operating System/Week 5/rearrange.py
+++ Google_IT_Automation_Specialization/C2 - Using Python to Interact with the Operating System/Week 5/rearrange.py
@@ -1,29 +1,11 @@
+#!/usr/bin/env python3
+
 import re
-
 result = re.search(r"^(\w*), (\w*)$", "Singh, Saurabh")
-print(result)
-print(result[0])
-print(result[1])
-print(result[2])
-
-print(f"{result[2]} {result[1]}")
-
-
-def rearranged_name(name):
-    result = re.search(r"^(\w*), (\w*)$", name)
-    if result is None:
-        return name
-    return f"{result[2]} {result[1]}"
-
-
-print(rearranged_name("Dostoevsky, Fyodor"))
-
-print(rearranged_name("Dostoevsky, F.M."))
-print(rearranged_name("Dostoevsky, Fyodor"))
 
 
 def rearrange_name(name):
-    result = re.search(r"^(\w*), ([a-zA-Z\. ]*)$", name)
+    result = re.search(r"^([\w. ]*), ([\w. ]*)$", name)
     if result == None:
         return None
     return "{} {}".format(result[2], result[1])
