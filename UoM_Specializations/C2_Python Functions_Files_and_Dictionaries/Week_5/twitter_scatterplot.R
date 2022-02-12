305cd84d42d5af42550262c8a087898511e6b49d
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-02-12 14:10:53 +0530

    modified r function for better scatter plot

--- UoM_Specializations/C2_Python Functions_Files_and_Dictionaries/Week_5/twitter_scatterplot.R
+++ UoM_Specializations/C2_Python Functions_Files_and_Dictionaries/Week_5/twitter_scatterplot.R
@@ -2,3 +2,14 @@ path <- "/Users/saurabhkumarsingh/Documents/GitHub/Learning_python/UoM_Specializ
 content <- read.csv(path)
 
 print(content)
+
+plot(content$Net.Score, content$Number.of.Retweets,
+     pch = 19,         # Solid circle
+     cex = 1.5,        # Make 150% size
+     col = "#cc0000",  # Red
+     main = "Twitter Sentiment Classifier",
+     xlab = "Net Score",
+     ylab = "Number of Retweets")
+     
+     
+     
