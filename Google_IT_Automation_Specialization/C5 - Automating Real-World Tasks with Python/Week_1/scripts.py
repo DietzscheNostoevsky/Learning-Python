fa7df4e031de29fdf2d1b9f8cb66fe0cf997bef0
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-03-15 18:44:13 +0530

    week 1 done

--- Google_IT_Automation_Specialization/C5 - Automating Real-World Tasks with Python/Week_1/scripts.py
+++ Google_IT_Automation_Specialization/C5 - Automating Real-World Tasks with Python/Week_1/scripts.py
@@ -1,3 +1,6 @@
+
+#!/usr/bin/env python3
+
 import PIL
 import os
 from PIL import Image
@@ -5,15 +8,15 @@ from PIL import Image
 parent = 'images'
 
 for filename in os.listdir('images'):
-    savedir = '/opt/icons/' + filename + '.jpeg'
+    savedir = '/opt/icons/' + filename
     if filename.endswith(".py"):
+        continue
+    elif filename.endswith('.DS_Store'):
         continue
     else:
         path = 'images' + '/' + filename
 
         with Image.open(path) as img:
 
-            img.rotate(90).resize((128, 128)).convert('RGB')
-            img.save("/opt/icons/" + filename, 'JPEG')
-        # except:
-        # continue
+            a = img.rotate(270).resize((128, 128)).convert('RGB')
+            a.save(savedir, 'JPEG')
