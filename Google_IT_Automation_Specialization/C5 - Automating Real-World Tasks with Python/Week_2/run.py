02bdf2d3a2b549414fbe923b0076c4e6f6cab4ed
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-03-20 01:08:18 +0530

    successfully opened files and read text

--- Google_IT_Automation_Specialization/C5 - Automating Real-World Tasks with Python/Week_2/run.py
+++ Google_IT_Automation_Specialization/C5 - Automating Real-World Tasks with Python/Week_2/run.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
+
+# %%
 import os
 import requests
 
-dir = '/data/feedback'
+dir = 'data/feedback'
 
 files = os.listdir(dir)
+
+# %%
+for file in files:
+    filepath = os.path.join(dir, file)
+    with open(filepath, 'r') as text:
+        lines = text.readlines()
+        postdict = dict()
+
+
+# %%
