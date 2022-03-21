c3e49e8ed270eede9cf227b07955086370c02267
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-03-21 13:01:59 +0530

    updated emails.py

--- Google_IT_Automation_Specialization/C5 - Automating Real-World Tasks with Python/Week_4/emails.py
+++ Google_IT_Automation_Specialization/C5 - Automating Real-World Tasks with Python/Week_4/emails.py
@@ -33,3 +33,15 @@ def send_email(message):
     mail_server = smtplib.SMTP('localhost')
     mail_server.send_message(message)
     mail_server.quit()
+
+
+def generate_error_email(sender, recipient, subject, body):
+    """Creates an email without an attachement."""
+    # Basic Email formatting
+    message = email.message.EmailMessage()
+    message["From"] = sender
+    message["To"] = recipient
+    message["Subject"] = subject
+    message.set_content(body)
+
+    return message
