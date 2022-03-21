528f792a009bbe7b6eef496996e18e9f896bf5a5
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-03-21 12:58:01 +0530

    done report emails .py

---
+++ Google_IT_Automation_Specialization/C5 - Automating Real-World Tasks with Python/Week_4/report_email.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# %%
+import os
+import datetime
+import reports
+import emails
+
+
+date = datetime.date.today().strftime("%B  %d, %Y")
+title = f'Processed update on {date}'
+filedir = 'supplier-data/descriptions'
+dirs = [os.path.join(filedir, file)
+        for file in os.listdir(filedir) if file.endswith('.txt')]
+
+dirs = sorted(dirs)
+fruits = []
+
+for file in dirs:
+    with open(file, 'r') as txt_data:
+        data = txt_data.readlines()
+        fruits.append((data[0].strip(), data[1].strip()))
+
+summary = ''
+br = '<br />'
+for fruitname, fruitweight in fruits:
+    summary += fruitname + br + fruitweight + br + br
+
+
+# %%
+if __name__ == "__main__":
+    reports.generate_report("/tmp/processed.pdf", title, summary)
+    sender = "automation@example.com"
+    receiver = "{}@example.com".format(os.environ.get('USER'))
+    subject = "Upload Completed - Online Fruit Store"
+    body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
+
+    message = emails.generate_email(
+        sender, receiver, subject, body, "/tmp/processed.pdf")
+    emails.send_email(message)
+
+# %%
