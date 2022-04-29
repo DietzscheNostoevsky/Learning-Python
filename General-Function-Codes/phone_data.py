a9b5404dd4e3a1d433d9f548f046e1184f561c8f
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-04-29 20:27:03 +0530

    date

--- General-Function-Codes/phone_data.py
+++ General-Function-Codes/phone_data.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python
+
+# To calculate how much data to remain at the end of the day
+# and how much data I can use per day
+
+# %%
+import datetime
+
+end_date = datetime.date(2022, 5, 20)
+
+end = end_date.strftime("%d-%m-%Y")
+
+today = datetime.date.today()
+# %%
+
+data_today = input("Enter data remaining : ")
