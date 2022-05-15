a8a64975f4c8730326bd2f96562faab1066182ba
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-05-15 19:26:36 +0530

    roman dict

--- Leetcode_py/roman_to_integer.py
+++ Leetcode_py/roman_to_integer.py
@@ -1,2 +1,17 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+
+        # defining variable values
+        roman_dict = {
+
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+
+        }
+
+        s = s.upper()
