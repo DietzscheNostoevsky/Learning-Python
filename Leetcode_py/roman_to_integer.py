49c310565952e5b7bd503f6c75ea510aed03453a
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-05-16 22:12:31 +0530

    update dict

--- Leetcode_py/roman_to_integer.py
+++ Leetcode_py/roman_to_integer.py
@@ -5,22 +5,23 @@ class Solution:
         roman_dict = {
 
             'I': 1,
-            'II': 2,
-            'III': 3,
-            'IV': 4,
+            # 'II': 2,
+            # 'III': 3,
+            # 'IV': 4,
             'V': 5,
-            'IX': 9,
+            # 'IX': 9,
             'X': 10,
-            'XL': 40,
+            # 'XL': 40,
             'L': 50,
-            'XC': 90,
+            # 'XC': 90,
             'C': 100,
-            'CD': 400,
+            # 'CD': 400,
             'D': 500,
-            'CM': 900,
+            # 'CM': 900,
             'M': 1000,
 
-
         }
+
+        # Problem is simpler to solve by working the string from back to front and using a map.
 
         s = s.upper()
