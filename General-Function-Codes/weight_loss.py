f8f37bb0c252ac28e3ce47d636cdf82ffcbb9ec0
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-05-22 07:13:03 +0530

    w input

--- General-Function-Codes/weight_loss.py
+++ General-Function-Codes/weight_loss.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python
+
+# First create a DB for weight tracking
+
+# %%
+present_weight = int(input("Insert present weight in KG : "))
+goal_weight = int(input("Insert Goal Weight : "))
+
+# %%
