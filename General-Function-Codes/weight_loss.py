67ae30a6c5fad33e795c1075d283575c44bee744
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-05-22 07:15:23 +0530

    time

--- General-Function-Codes/weight_loss.py
+++ General-Function-Codes/weight_loss.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
 # First create a DB for weight tracking
+# %%
+import datetime
+from datetime import timedelta
+
+end_date = datetime.date(2022, 6, 5)
+
+end = end_date.strftime("%d-%m-%Y")
+
+today = datetime.date.today()
 
 # %%
 present_weight = int(input("Insert present weight in KG : "))
