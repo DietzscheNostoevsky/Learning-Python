43a965e35b07ae7bacb8e08d6b8989616175337f
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-06-19 20:48:23 +0530

    changed default to 85

--- General-Function-Codes/WeightGraph.py
+++ General-Function-Codes/WeightGraph.py
@@ -32,7 +32,7 @@ print("Welcome to Weight Reduce Generator")
 print("It shows how much weight you will lose in coming days")
 print("If you lose 1 % of your weight per week")
 
-present_weight = int(input("Input present weight, default 84 : ") or "84")
+present_weight = int(input("Input present weight, default 85 : ") or "84")
 today = datetime.date.today()
 
 print(today)
