14a65b66b7d02faacefeeec78730ddbcd7689699
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-07-04 23:52:03 +0530

    Update fibonacci.py

--- General-Function-Codes/fibonacci.py
+++ General-Function-Codes/fibonacci.py
@@ -1,15 +1,17 @@
-#Fibonacchi implementation using Recursion
+#!/usr/bin/env python
+# Fibonacchi implementation using Recursion
 # fibo(n) is the nth value of the sequence
-#fibo(n) calculates the nth value 
+# fibo(n) calculates the nth value
 
-def fibo(n) :
-    
-    if n==0 :
+def fibo(n):
+
+    if n == 0:
         return 0
-    elif n==1 or n==2 : 
+    elif n == 1 or n == 2:
         return 1
-    else : 
-        return ( fibo(n-1) + fibo(n-2))
+    else:
+        return (fibo(n-1) + fibo(n-2))
 
-for i in range (5):
+
+for i in range(5):
     print(fibo(i), end=" ")
