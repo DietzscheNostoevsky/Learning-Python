e22673dd0f76e5b23f7e37ccb2c8b290e8ad0b91
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-12-13 19:29:21 +0530

    documentation

--- General-Function-Codes/focus.py
+++ General-Function-Codes/focus.py
@@ -1,1 +1,7 @@
 # improved focusing time based on Huberman videos
+#
+# Not Pomodoro
+# Focus for 90 minutes Ultradian rhythm
+# 90 minute implementd using Pomodoro Timer
+#
+# Implementing random microrests using Python
